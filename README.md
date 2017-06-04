a9685bf49b5100c0c40cb008bea9a6365620d92c
Alexey Pushkov <pushkov@inbox.ru>
2017-06-04 12:53:56 +0300

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # otus_java_2017_06_alexey_pushkov
 Homework for Java Course at otus.
+
+author
+Alexey Pushkov (Алексей Пушков)
+
+pushkov@inbox.ru
---
+++ l1maven/src/main/java/ru/otus/l1/Alert.java
@@ -0,0 +1,10 @@
+package ru.otus.l1;
+
+/**
+ * Created by carapooh on 04.06.2017.
+ */
+public class Alert {
+    public static void main(String[] args){
+        System.out.println("ALERT!");
+    }
+}
